594baa5d8f0347216ee10b2e9f14d1aa7446586b
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-04 17:42:24 +0100

    Remove redundancies

--- java-example/src/java/example/Example.java
+++ java-example/src/java/example/Example.java
@@ -8,26 +8,6 @@ import static java.example.Apple.COLOR.GREEN;
 
 public class Example {
 
-    public static List<Apple> filterGreenApples(List<Apple> inventory) {
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple: inventory) {
-            if (GREEN.equals(apple.getColor())) {
-                result.add(apple);
-            }
-        }
-        return result;
-    }
-
-    public static List<Apple> filterHeavyApples(List<Apple> inventory) {
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple: inventory) {
-            if (apple.getWeight() > 150) {
-                result.add(apple);
-            }
-        }
-        return result;
-    }
-
     static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
         List<Apple> result = new ArrayList<>();
         for (Apple apple: inventory) {
@@ -41,10 +21,8 @@ public class Example {
     public static void main(String[] args) {
         List<Apple> inventory = new ArrayList<>();
 
-        filterGreenApples(inventory);
         filterApples(inventory, Apple::isGreenApple);
 
-        filterHeavyApples(inventory);
         filterApples(inventory, Apple::isHeavyApple);
     }
 }
