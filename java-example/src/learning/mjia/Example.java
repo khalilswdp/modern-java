aef3b60d66ae22d9c1203d5fde9f51c10463d74a
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-06 10:11:06 +0100

    More lambda expression stuff

--- java-example/src/learning/mjia/Example.java
+++ java-example/src/learning/mjia/Example.java
@@ -79,6 +79,22 @@ public class Example {
 
         // This is how we can have a variable, honestly wtf! java 8+ is so cool!
         ApplePredicate specialApplePredicate = (Apple apple) -> apple.getWeight() > 150 && apple.getColor() != RED;
+
+        Runnable r1 = () -> System.out.println("Hello World 1");
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello World 2");
+            }
+        };
+
+        process(r1);
+        process(r2);
+        process (() -> System.out.println("Hello World 3"));
+    }
+
+    public static void process(Runnable r) {
+        r.run();
     }
 
     public static <T> List <T> filter(List<T> list, Predicate<T> p) {
