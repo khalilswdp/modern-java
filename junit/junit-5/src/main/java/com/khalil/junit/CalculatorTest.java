dba1b55227ce07e1179d6f769795f57b715bc32c
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-22 12:38:59 +0100

    Initial commit + simple test

---
+++ junit/junit-5/src/main/java/com/khalil/junit/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.khalil.junit;
+
+class CalculatorTest {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        double result = calculator.add(10, 50);
+        if (result != 60) {
+            System.out.println("Bad result: " + result);
+        }
+    }
+
+}
