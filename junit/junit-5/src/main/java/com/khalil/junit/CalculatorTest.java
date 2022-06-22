a779c12433f3074afdf63cee0951cc83db60fb0a
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-22 12:53:22 +0100

    Minor changes to the test

--- junit/junit-5/src/main/java/com/khalil/junit/CalculatorTest.java
+++ junit/junit-5/src/main/java/com/khalil/junit/CalculatorTest.java
@@ -2,12 +2,28 @@ package com.khalil.junit;
 
 class CalculatorTest {
 
-    public static void main(String[] args) {
+    private int nbErrors = 0;
+
+    public void testAdd() {
         Calculator calculator = new Calculator();
         double result = calculator.add(10, 50);
         if (result != 60) {
-            System.out.println("Bad result: " + result);
+            throw new IllegalStateException("Bad result: " + result);
         }
+    }
+
+    public static void main(String[] args) {
+        CalculatorTest test = new CalculatorTest();
+        try {
+            test.testAdd();
+        } catch (Throwable e) {
+            test.nbErrors++;
+            e.printStackTrace();
+        }
+        if (test.nbErrors > 0) {
+            throw new IllegalStateException("There were " + test.nbErrors + " error(s)");
+        }
+
     }
 
 }
