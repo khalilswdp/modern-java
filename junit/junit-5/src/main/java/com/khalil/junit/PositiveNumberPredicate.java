bcf7ddb74c788e83442557afe9290bba8adcd50c
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-24 15:50:24 +0100

    Dynamic tests where the Executables are called one after another as a single test which are created dynamically at runtime (not known in advance)

---
+++ junit/junit-5/src/main/java/com/khalil/junit/PositiveNumberPredicate.java
@@ -0,0 +1,7 @@
+package com.khalil.junit;
+
+public class PositiveNumberPredicate {
+    public boolean check(int number) {
+        return number > 0;
+    }
+}
--- junit/junit-5/src/main/java/com/khalil/junit/WordCounter.java
+++
@@ -1,7 +0,0 @@
-package com.khalil.junit;
-
-public class WordCounter {
-    public int countWords(String sentence) {
-        return sentence.split(" ").length;
-    }
-}
