4f4174a8f2803770e6649baaa9139871bc2c61aa
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-24 14:58:00 +0100

    Parameterize a test with a list of your test method's attributes as Comma-Separated Values read from a file

---
+++ junit/junit-5/src/test/java/com/khalil/junit/ParameterizedWithCsvFileSourceTest.java
@@ -0,0 +1,16 @@
+package com.khalil.junit;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParameterizedWithCsvFileSourceTest {
+    private WordCounter wordCounter = new WordCounter();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/word_counter.csv")
+    void testWordsInSentence(int expected, String sentence) {
+        assertEquals(expected, wordCounter.countWords(sentence));
+    }
+}
