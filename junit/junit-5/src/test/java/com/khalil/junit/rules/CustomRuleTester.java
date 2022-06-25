d9e9ba4800434de6cf7eac2db6e6ed7e997895d6
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-25 22:38:27 +0100

    Provide customrules for tests, that is, change the default way tests run (for example, print something before or after, or do particular things)...

---
+++ junit/junit-5/src/test/java/com/khalil/junit/rules/CustomRuleTester.java
@@ -0,0 +1,15 @@
+package com.khalil.junit.rules;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CustomRuleTester {
+
+    @Rule
+    public CustomRule myRule = new CustomRule();
+
+    @Test
+    public void myCustomRuleTest() {
+        System.out.println("Call of a test method");
+    }
+}
---
+++ junit/junit-5/src/test/java/com/khalil/junit/rules/CustomRuleTester2.java
@@ -0,0 +1,19 @@
+package com.khalil.junit.rules;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CustomRuleTester2 {
+
+    private CustomRule myRule = new CustomRule();
+
+    @Rule
+    public CustomRule getMyRule() {
+        return myRule;
+    }
+
+    @Test
+    public void myCustomRuleTest() {
+        System.out.println("Call of a test method");
+    }
+}
