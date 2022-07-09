aa4191b1f396b83171ebaba102a42dd7552bb72e
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-07-09 21:18:08 +0100

    Proving that the custom collector is way faster than partitioning by old is prime predicate with no history

--- java-example/src/learning/mjia/Example.java
+++ java-example/src/learning/mjia/Example.java
@@ -12,9 +12,23 @@ public class Example {
     // Review  (High value items): 3.3, 5.8 (files), 6.2 and 6.3
 
     public static void main(String[] args) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            partitionPrimes(1_000_000);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            if (duration < fastest) fastest = duration;
+        }
+        System.out.println("Fastest execution done by old method in " + fastest + " msecs");
 
-        Map<Boolean, List<Integer>> booleanListMap = partitionPrimesWithCustomCollector(100);
-        System.out.println(booleanListMap);
+        fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            partitionPrimesWithCustomCollector(1_000_000);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            if (duration < fastest) fastest = duration;
+        }
+        System.out.println("Fastest execution done by customCollector method in " + fastest + " msecs");
 
         // 6.6
     }
@@ -22,5 +36,16 @@ public class Example {
     public static Map<Boolean, List<Integer>> partitionPrimesWithCustomCollector(int n) {
         return IntStream.rangeClosed(2, n).boxed()
                 .collect(new PrimeNumbersCollector());
+    }
+
+    public static Map<Boolean, List<Integer>> partitionPrimes(int n) {
+        return IntStream.rangeClosed(2, n).boxed()
+                .collect(partitioningBy(candidate -> isPrime(candidate)));
+    }
+
+    public static boolean isPrime(int candidate) {
+        return IntStream.rangeClosed(2, candidate-1)
+                .limit((long) Math.floor(Math.sqrt(candidate)) - 1)
+                .noneMatch(i -> candidate % i == 0);
     }
 }
