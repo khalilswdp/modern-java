a454042e8d9da30602eed9f44869c52c9404c69d
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-09-06 17:44:52 +0100

    Major clean up

---
+++ junit/junit-5/src/test/java/com/khalil/junit/assumptions/JUnit4SUTTest.java
@@ -0,0 +1,51 @@
+package com.khalil.junit.assumptions;
+
+import com.khalil.junit.assumptions.lifecycle.ResourceForAllTests;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class JUnit4SUTTest {
+
+    private static ResourceForAllTests resourceForAllTests;
+    private SUT systemUnderTest;
+
+    @BeforeClass
+    public static void setUpClass() {
+        resourceForAllTests = new ResourceForAllTests("Our resource for all tests");
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        resourceForAllTests.close();
+    }
+
+    @Before
+    public void setUp() {
+        systemUnderTest = new SUT("Our system under test");
+    }
+
+    @After
+    public void tearDown() {
+        systemUnderTest.close();
+    }
+
+    @Test
+    public void testRegularWork() {
+        boolean canReceiveRegularWork = systemUnderTest.canReceiveRegularWork();
+        assertTrue(canReceiveRegularWork);
+    }
+
+    @Test
+    public void testAdditionalWork() {
+        boolean canReceiveAdditionalWork = systemUnderTest.canReceiveAdditionalWork();
+        assertFalse(canReceiveAdditionalWork);
+    }
+
+    @Test
+    @Ignore
+    public void myThirdTest() {
+        assertEquals("2 is not equal to 1", 2, 1);
+    }
+
+}
