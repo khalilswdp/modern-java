20916b036ace7c10fa1f77e691c6614164e97563
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-10-05 03:20:03 +0100

    ZigZag Conversion Optimized to the maximum

--- java-example/src/learning/mjia/Example.java
+++ java-example/src/learning/mjia/Example.java
@@ -1,7 +1,5 @@
 package learning.mjia;
 
-import java.util.Arrays;
-
 public class Example {
 
     public static void main(String[] args) {
@@ -15,30 +13,20 @@ public class Example {
             return s;
         }
 
-        String[] rows = new String[numRows];
-        Arrays.fill(rows, "");
         int zigZagLength = 2 * (numRows - 1);
 
-        // two possible alternatives (I watched 3 videos, and they're all equally inspiring)!
-        // Loop through the rows! and within do some of your magic!
-        // An alternative: just loop through the string and keep track of the row as you go (two approaches: boolean, or not/string builder)
 
+        StringBuilder allRows = new StringBuilder(stringLength);
         for (int row = 0; row < numRows; row++) {
-            for (int indexAtZig = row; indexAtZig < stringLength; indexAtZig+= zigZagLength) {
-                char[] currentRowAppend = new char[2];
-                char characterInZig;
-                characterInZig = s.charAt(indexAtZig);
-                currentRowAppend[0] = characterInZig;
-                if (!(row == 0 || row == numRows - 1)) {
-                    int indexAtZag = indexAtZig + zigZagLength - 2 * row;
-                    if (indexAtZag < stringLength) {
-                        currentRowAppend[1] = s.charAt(indexAtZag);
-                    }
+            for (int indexAtZig = row; indexAtZig < stringLength; indexAtZig += zigZagLength) {
+                allRows.append(s.charAt(indexAtZig));
+                int indexAtZag = indexAtZig + zigZagLength - 2 * row;
+                if (!(row == 0 || row == numRows - 1 || indexAtZag >= stringLength)) {
+                    allRows.append(s.charAt(indexAtZag));
                 }
-                rows[row] += new String(currentRowAppend).trim();
             }
         }
-        return String.join("", rows);
+        return allRows.toString();
 
     }
 }
