1732cd6ba8284c13cb1eebadc84223a9d48b1e16
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-14 12:01:26 +0100

    Two Sum: O(n) Fastest Solution, One of the best approaches

---
+++ java-example/src/learning/mjia/leetcode/grind75/TwoSumN.java
@@ -0,0 +1,17 @@
+package learning.mjia.leetcode.grind75;
+
+import java.util.HashMap;
+
+public class TwoSumN {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> leftToIndex = new HashMap<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (leftToIndex.containsKey(nums[index])) {
+                return new int[] {index, leftToIndex.get(nums[index])};
+            } else {
+                leftToIndex.put(target - nums[index], index);
+            }
+        }
+        return null;
+    }
+}
