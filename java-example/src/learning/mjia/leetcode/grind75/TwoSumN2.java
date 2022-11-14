dc38d8ce72c9993fdfb30c537cb327f94d0fbef4
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-14 11:55:22 +0100

    Two Sum: O(n²) Brute-force, One of the worst approaches

---
+++ java-example/src/learning/mjia/leetcode/grind75/TwoSumN2.java
@@ -0,0 +1,15 @@
+package learning.mjia.leetcode.grind75;
+
+public class TwoSumN2 {
+    public int[] twoSum(int[] nums, int target) {
+        int numsLength = nums.length;
+        for (int i = 1; i < numsLength; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
