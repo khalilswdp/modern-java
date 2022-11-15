454ef636fc2a914e2f1a9c028c2dcbbd4da7f870
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-16 00:35:01 +0100

    Valid Anagram: O(n) High level approach

---
+++ java-example/src/learning/mjia/leetcode/grind75/validanagram/ValidAnagram.java
@@ -0,0 +1,36 @@
+package learning.mjia.leetcode.grind75.validanagram;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> sMap = fill(s), tMap = fill(t);
+        if (!(sMap.keySet().equals(tMap.keySet()))) {
+            return false;
+        }
+        for (Character character : sMap.keySet()) {
+            if (sMap.get(character).compareTo(tMap.get(character)) != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Map<Character, Integer> fill(String s) {
+        Map<Character, Integer> result = new HashMap<>();
+        char[] characters = s.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            result.put(characters[i], result.getOrDefault(characters[i], 0) + 1);
+        }
+        return result;
+    }
+}
