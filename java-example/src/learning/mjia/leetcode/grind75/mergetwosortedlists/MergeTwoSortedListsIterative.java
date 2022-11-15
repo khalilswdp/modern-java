751fcdb41094714f0572bcc145540b9baf2bdee1
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-15 16:30:23 +0100

    Merge Two Sorted Lists: O(n) Iterative Approach

---
+++ java-example/src/learning/mjia/leetcode/grind75/mergetwosortedlists/MergeTwoSortedListsIterative.java
@@ -0,0 +1,31 @@
+package learning.mjia.leetcode.grind75.mergetwosortedlists;
+
+public class MergeTwoSortedListsIterative {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = null;
+        ListNode currentIteration = null;
+        while (isOneOfTheListsNotNull(list1, list2)) {
+            ListNode newOne = new ListNode();
+            if (list1 == null || (list2 != null && list1.val >= list2.val)) {
+                newOne.val = list2.val;
+                list2 = list2.next;
+            } else if (list2 == null || (list1 != null && list1.val < list2.val)) {
+                newOne.val = list1.val;
+                list1 = list1.next;
+            }
+            if (result == null) {
+                result = newOne;
+                currentIteration = result;
+            } else {
+                currentIteration.next = newOne;
+                currentIteration = currentIteration.next;
+            }
+        }
+        return result;
+    }
+
+    private static boolean isOneOfTheListsNotNull(ListNode list1, ListNode list2) {
+        return list1 != null || list2 != null;
+    }
+}
+
