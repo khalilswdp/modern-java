d15c5fe04ed05ec3d95b2b4c00ee41a8a5c3eaf4
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-15 23:21:45 +0100

    InvertBinaryTree: O(n) The intuitive approach is also the most efficient

---
+++ java-example/src/learning/mjia/leetcode/grind75/invertbinarytree/InvertBinaryTree.java
@@ -0,0 +1,15 @@
+package learning.mjia.leetcode.grind75.invertbinarytree;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tempo = root.left;
+        root.left = root.right;
+        root.right = tempo;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
