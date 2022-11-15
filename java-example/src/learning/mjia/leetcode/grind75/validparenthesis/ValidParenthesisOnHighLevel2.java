8f7bcf8e3c2d1b8170f9956a9cf3484b0204cd78
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-15 14:14:41 +0100

    Valid Parenthesis: O(n) High level approach 2

---
+++ java-example/src/learning/mjia/leetcode/grind75/validparenthesis/ValidParenthesisOnHighLevel2.java
@@ -0,0 +1,32 @@
+package learning.mjia.leetcode.grind75.validparenthesis;
+
+import java.util.Stack;
+
+public class ValidParenthesisOnHighLevel2 {
+
+    public static void main(String[] args) {
+        isValid("({[]})");
+    }
+    public static boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> characterQueue = new Stack<>();
+
+
+        char[] chars = s.toCharArray();
+        for (Character c: chars) {
+            if (c == '(' || c == '{' || c == '[') {
+                characterQueue.add(c);
+            } else if (!characterQueue.isEmpty() && ((c == ')' && characterQueue.peek() == '(')
+                    || (c == ']' && characterQueue.peek() == '[')
+                    || (c == '}' && characterQueue.peek() == '{'))) {
+                characterQueue.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return characterQueue.isEmpty();
+    }
+}
