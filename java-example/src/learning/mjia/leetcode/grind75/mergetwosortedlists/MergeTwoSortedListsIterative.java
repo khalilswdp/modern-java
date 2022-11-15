f3d39c5d1b8d479a2f66f37f01827f48e3a036ff
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-15 17:20:24 +0100

    Use dummy to improve performance

--- java-example/src/learning/mjia/leetcode/grind75/mergetwosortedlists/MergeTwoSortedListsIterative.java
+++ java-example/src/learning/mjia/leetcode/grind75/mergetwosortedlists/MergeTwoSortedListsIterative.java
@@ -2,26 +2,18 @@ package learning.mjia.leetcode.grind75.mergetwosortedlists;
 
 public class MergeTwoSortedListsIterative {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result = null;
-        ListNode currentIteration = null;
+        ListNode result = new ListNode(), currentIteration = result;
         while (isOneOfTheListsNotNull(list1, list2)) {
-            ListNode newOne = new ListNode();
             if (isSecondSmaller(list1, list2)) {
-                newOne.val = list2.val;
+                currentIteration.next = list2;
                 list2 = list2.next;
             } else if (isSecondSmaller(list2, list1)) {
-                newOne.val = list1.val;
+                currentIteration.next = list1;
                 list1 = list1.next;
             }
-            if (result == null) {
-                result = newOne;
-                currentIteration = result;
-            } else {
-                currentIteration.next = newOne;
-                currentIteration = currentIteration.next;
-            }
+            currentIteration = currentIteration.next;
         }
-        return result;
+        return result.next;
     }
 
     private static boolean isSecondSmaller(ListNode list1, ListNode list2) {
