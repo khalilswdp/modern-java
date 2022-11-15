a2646b5eae03cac3c34d13033d84f632ed41a90d
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-15 17:37:29 +0100

    Improve maintainability of code

---
+++ java-example/src/learning/mjia/leetcode/grind75/validparenthesis/ValidParenthesisOnHighLevelGeneric.java
@@ -0,0 +1,34 @@
+package learning.mjia.leetcode.grind75.validparenthesis;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParenthesisOnHighLevelGeneric {
+
+    public static void main(String[] args) {
+        isValid("({[]})");
+    }
+    public static boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> characterQueue = new Stack<>();
+
+        Map<Character, Character> openToClosed = new HashMap<>();
+        openToClosed.put('(', ')');
+        openToClosed.put('{', '}');
+        openToClosed.put('[', ']');
+
+        char[] chars = s.toCharArray();
+        for (Character c: chars) {
+            if (openToClosed.containsKey(c)) {
+                characterQueue.push(openToClosed.get(c));
+            } else if (characterQueue.isEmpty() || c != characterQueue.pop()) {
+                return false;
+            }
+        }
+
+        return characterQueue.isEmpty();
+    }
+}
