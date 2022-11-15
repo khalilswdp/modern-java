e04fe979d5ea9440c3ee104d2164ceb521342181
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-15 17:10:55 +0100

    More efficient solution

---
+++ java-example/src/learning/mjia/leetcode/grind75/mergetwosortedlists/MergeTwoSortedListsIterativeEfficient.java
@@ -0,0 +1,45 @@
+package learning.mjia.leetcode.grind75.mergetwosortedlists;
+
+public class MergeTwoSortedListsIterativeEfficient {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode result, currentIteration;
+        if (list1.val >= list2.val) {
+            currentIteration = list2;
+            list2 = list2.next;
+        } else {
+            currentIteration = list1;
+            list1 = list1.next;
+        }
+        result = currentIteration;
+
+        while (bothNotNull(list1, list2)) {
+            if (list1.val >= list2.val) {
+                currentIteration.next = list2;
+                list2 = list2.next;
+            } else {
+                currentIteration.next = list1;
+                list1 = list1.next;
+            }
+            currentIteration = currentIteration.next;
+        }
+
+        if (list1 != null) {
+            currentIteration.next = list1;
+        } else {
+            currentIteration.next = list2;
+        }
+
+        return result;
+    }
+
+    private static boolean bothNotNull(ListNode list1, ListNode list2) {
+        return list1 != null && list2 != null;
+    }
+}
+
