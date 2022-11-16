013a397d7200cb7d5180161a7e628233df14f94e
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-16 12:22:07 +0100

    Valid Anagram: O(n) Low level approach

---
+++ java-example/src/learning/mjia/leetcode/grind75/validanagram/ValidAnagramLowLevel.java
@@ -0,0 +1,34 @@
+package learning.mjia.leetcode.grind75.validanagram;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagramLowLevel {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+    public static boolean isAnagram(String s, String t) {
+        int sLength = s.length();
+        if (sLength != t.length()) {
+            return false;
+        }
+
+        int[] characters = new int[26]; // to be changed to fit all unicode characters
+
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        for (int i = 0; i < sLength; i++) {
+            characters[sChars[i] - 'a']++;
+            characters[tChars[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (characters[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
