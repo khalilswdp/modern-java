1b9f54255adc483a61612b44aab460b32b7efa9d
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-16 13:30:59 +0100

    Improve memory consumption

--- java-example/src/learning/mjia/leetcode/grind75/binarysearch/BinarySearch.java
+++ java-example/src/learning/mjia/leetcode/grind75/binarysearch/BinarySearch.java
@@ -2,8 +2,7 @@ package learning.mjia.leetcode.grind75.binarysearch;
 
 public class BinarySearch {
     public int search(int[] nums, int target) {
-        int numsLength = nums.length;
-        int left = 0, right = numsLength -1;
+        int left = 0, right = nums.length -1;
 
         while (left <= right) {
             int mid = (left + right) / 2;
