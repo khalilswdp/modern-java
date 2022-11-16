0b39f27ae7b5f4d9df55042a9d6aa25a8c812b85
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-16 14:59:45 +0100

    minor improvement

--- java-example/src/learning/mjia/leetcode/grind75/floodfill/FloodFill.java
+++ java-example/src/learning/mjia/leetcode/grind75/floodfill/FloodFill.java
@@ -10,6 +10,9 @@ public class FloodFill {
     }
 
     private void floodFillImage(int[][] image, int sr, int sc, int originalColor, int color) {
+        // we can also do an if outside of image return; else, send problem to all 4 ways
+        // if !(sr < 0 || sr >= imageRowLength || sc < 0 || sc >= image[0].length || image[sr][sc] != originalColor) {
+        // And remove the inner ifs
         if (image[sr][sc] == originalColor) {
             image[sr][sc] = color;
 
