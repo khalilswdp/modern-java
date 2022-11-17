a5864e5241ba94eff5603afe6ee7c29adeb1f855
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-17 22:20:40 +0100

    Less readable alternative

---
+++ java-example/src/learning/mjia/leetcode/grind75/maximumsubarray/MaximumSubarrayDivideAndConquerV2OnLessMemoryLessReadable.java
@@ -0,0 +1,29 @@
+package learning.mjia.leetcode.grind75.maximumsubarray;
+
+public class MaximumSubarrayDivideAndConquerV2OnLessMemoryLessReadable {
+    public static void main(String[] args) {
+        maxSubArray(new int[]{8,-19,5,-4,20});
+    }
+    public static int maxSubArray(int[] nums) {
+        return findMaximumSubarrayByComparingMSDs(nums, 0, nums.length - 1)[1];
+    }
+
+    private static int[] findMaximumSubarrayByComparingMSDs(int[] nums, int start, int end) {
+        if (start == end) {
+            return new int[] { nums[start], nums[start], nums[start], nums[start] };
+        } else {
+            int mid = start + (end - start) / 2;
+            return compareMSDs(findMaximumSubarrayByComparingMSDs(nums, start, mid),
+                    findMaximumSubarrayByComparingMSDs(nums, mid + 1, end));
+        }
+    }
+
+    private static int[] compareMSDs(int[] left, int[] right) {
+        return new int[] {
+                left[0] + right[0],
+                Math.max(left[3] + right[2],
+                        Math.max(left[1], right[1])),
+                Math.max(left[2], left[0] + right[2]),
+                Math.max(right[3], right[0] + left[3]) };
+    }
+}
