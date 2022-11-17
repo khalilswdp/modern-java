7635ab79916859170f3bfc3607256a15c136149d
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-17 20:38:56 +0100

    Maximum Subarray: O(nLogn) Divide and Conquer solution

---
+++ java-example/src/learning/mjia/leetcode/grind75/maximumsubarray/MaximumSubarrayDivideAndConquerOnLogn.java
@@ -0,0 +1,47 @@
+package learning.mjia.leetcode.grind75.maximumsubarray;
+
+public class MaximumSubarrayDivideAndConquerOnLogn {
+    public static void main(String[] args) {
+        maxSubArray(new int[]{8,-19,5,-4,20});
+    }
+    public static int maxSubArray(int[] nums) {
+        return maxSumInSubArray(nums, 0, nums.length - 1);
+    }
+
+    // maxSum such that mid is not part of it (both first ifs)
+    private static int maxSumInSubArray(int[] nums, int start, int end) {
+        if (start > end) {
+            return Integer.MIN_VALUE;
+        } else if (start == end) {
+            return nums[start];
+        } else {
+            int mid = start + (end - start) / 2;
+            return Math.max(maxSumInCrossedArray(nums, start, end, mid),
+                    Math.max(maxSumInSubArray(nums, start, mid - 1), maxSumInSubArray(nums, mid + 1, end)));
+        }
+    }
+
+    // maxSum such that mid is part of it
+    private static int maxSumInCrossedArray(int[] nums, int start, int end, int mid) {
+
+
+        int sum = 0;
+        int currentLeftSum = Integer.MIN_VALUE;
+        for (int i = mid; i >= start; i--) {
+            sum += nums[i];
+            currentLeftSum = Math.max(currentLeftSum, sum);
+        }
+
+        sum = 0;
+        int currentRightSum = Integer.MIN_VALUE;
+        for (int i = mid; i <= end; i++) {
+            sum += nums[i];
+            currentRightSum = Math.max(currentRightSum, sum);
+        }
+
+        // We are sure that it is indeed "currentRightSum + currentLeftSum - nums[mid]" because we start from the mid
+        // and count the sums outwards
+        return Math.max(currentRightSum + currentLeftSum - nums[mid],
+                Math.max(currentLeftSum, currentRightSum));
+    }
+}
