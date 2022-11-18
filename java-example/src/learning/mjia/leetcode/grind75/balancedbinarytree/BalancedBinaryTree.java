b04d2fcb3e82c0c96586061922ebb96b64f40f1a
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-18 12:29:50 +0100

    Balanced Binary Tree: O(logn) First Solution

---
+++ java-example/src/learning/mjia/leetcode/grind75/balancedbinarytree/BalancedBinaryTree.java
@@ -0,0 +1,36 @@
+package learning.mjia.leetcode.grind75.balancedbinarytree;
+
+public class BalancedBinaryTree {
+    public static void main(String[] args) {
+    }
+    public static boolean isBalanced(TreeNode root) {
+        return treeHeight(root).isBalanced;
+    }
+
+
+    private static BBT treeHeight(TreeNode root) {
+        if (root == null) {
+            return new BBT(true, 0);
+        } else if (root.left == null && root.right == null) {
+            return new BBT(true, 1);
+        } else {
+            BBT bbtLeft = treeHeight(root.left);
+            BBT bbtRight = treeHeight(root.right);
+            return new BBT(bbtLeft.isBalanced
+                    && bbtRight.isBalanced
+                    && Math.abs(bbtLeft.height - bbtRight.height) < 2,
+                    1 + Math.max(bbtLeft.height, bbtRight.height));
+        }
+    }
+
+    static class BBT {
+        boolean isBalanced;
+        int height;
+
+        public BBT(boolean isBalanced, int height) {
+            this.isBalanced = isBalanced;
+            this.height = height;
+        }
+    }
+}
+
