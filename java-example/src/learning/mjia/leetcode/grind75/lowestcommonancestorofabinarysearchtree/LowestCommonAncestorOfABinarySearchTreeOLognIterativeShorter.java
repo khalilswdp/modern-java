3a1d1c761f80a14774f7b1d423e4b910d2cfc881
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-18 11:26:48 +0100

    Use basic math to minimize amount of if elses

---
+++ java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/LowestCommonAncestorOfABinarySearchTreeOLognIterativeShorter.java
@@ -0,0 +1,16 @@
+package learning.mjia.leetcode.grind75.lowestcommonancestorofabinarysearchtree;
+
+public class LowestCommonAncestorOfABinarySearchTreeOLognIterativeShorter {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode current = root;
+        while (areSameSign(current.val - p.val, current.val - q.val)) {
+            current =  p.val > current.val ? current.right : current.left;
+        }
+        return current;
+    }
+
+    private static boolean areSameSign(int a, int b) {
+        return ((a < 0) && (b < 0)) || ((a > 0) && (b > 0));
+    }
+}
+
