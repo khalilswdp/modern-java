520038ac74e6ef41bcf25404431333736429b4ea
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-18 10:36:03 +0100

    Lowest Common Ancestor of a Binary Search Tree: O(Logn)

---
+++ java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,22 @@
+package learning.mjia.leetcode.grind75.lowestcommonancestorofabinarysearchtree;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val == q.val) {
+            return p;
+        } else {
+            return getLowestCommonTreeNode(root, p, q);
+        }
+    }
+
+    private TreeNode getLowestCommonTreeNode(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.left != null && p.val < root.val && q.val < root.val) {
+            return getLowestCommonTreeNode(root.left, p, q);
+        } else if (root.right != null && p.val > root.val && q.val > root.val) {
+            return getLowestCommonTreeNode(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
+
---
+++ java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/TreeNode.java
@@ -0,0 +1,11 @@
+package learning.mjia.leetcode.grind75.lowestcommonancestorofabinarysearchtree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
