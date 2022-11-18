a70d849ee1bca010e4d620aedff28a2bc5f67ddf
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-18 10:56:51 +0100

    Improve recursive approach (add more base cases to avoid messy boolean expressions)

--- java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/LowestCommonAncestorOfABinarySearchTreeOLognRecursive.java
+++ java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/LowestCommonAncestorOfABinarySearchTreeOLognRecursive.java
@@ -2,18 +2,12 @@ package learning.mjia.leetcode.grind75.lowestcommonancestorofabinarysearchtree;
 
 public class LowestCommonAncestorOfABinarySearchTreeOLognRecursive {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (p.val == q.val) {
-            return p;
-        } else {
-            return getLowestCommonTreeNode(root, p, q);
-        }
-    }
-
-    private TreeNode getLowestCommonTreeNode(TreeNode root, TreeNode p, TreeNode q) {
-        if (root.left != null && p.val < root.val && q.val < root.val) {
-            return getLowestCommonTreeNode(root.left, p, q);
-        } else if (root.right != null && p.val > root.val && q.val > root.val) {
-            return getLowestCommonTreeNode(root.right, p, q);
+        if (root == null) {
+            return null;
+        } else if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
         } else {
             return root;
         }
