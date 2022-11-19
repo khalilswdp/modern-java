5bfba5e26ea3949cffacea3f9b3e01843126c849
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-19 09:52:31 +0100

    Linked List Cycle: O(n) Naîve approach written simpler

---
+++ java-example/src/learning/mjia/leetcode/grind75/balancedbinarytree/BalancedBinaryTreeSimpler.java
@@ -0,0 +1,24 @@
+package learning.mjia.leetcode.grind75.balancedbinarytree;
+
+public class BalancedBinaryTreeSimpler {
+    private static boolean balance;
+
+    public static boolean isBalanced(TreeNode root) {
+        balance = true;
+        height(root);
+        return balance;    }
+
+    private static int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.right == null && root.left == null) {
+            return 1;
+        } else {
+            int heightRight = height(root.right);
+            int heightLeft = height(root.left);
+            balance = balance && Math.abs(heightRight - heightLeft) < 2;
+            return 1 + Math.max(heightRight, heightLeft);
+        }
+    }
+}
+
