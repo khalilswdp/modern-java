cc8501f7c472c681c63c87e16dd72a996cc66a24
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-20 22:59:54 +0100

    Remove unnecessary offset

--- java-example/src/learning/mjia/leetcode/grind75/insertinterval/InsertInterval.java
+++ java-example/src/learning/mjia/leetcode/grind75/insertinterval/InsertInterval.java
@@ -14,21 +14,19 @@ public class InsertInterval {
     public static int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> result = new ArrayList<>();
         int currentIndex = 0;
-        int offset = 0;
         for (int[] interval: intervals) {
             if (newInterval[1] < interval[0]) {
                 result.add(newInterval);
-                for (int i = currentIndex + offset; i < intervals.length; i++) {
+                for (int i = currentIndex; i < intervals.length; i++) {
                     result.add(intervals[i]);
                 }
                 return result.toArray(new int[result.size()][2]);
             } else if (newInterval[0] > interval[1]) {
                 result.add(interval);
-                currentIndex++;
             } else {
                 newInterval = new int[] { Math.min(newInterval[0], interval[0]), Math.max(newInterval[1], interval[1]) };
-                offset++;
             }
+            currentIndex++;
         }
 
         result.add(newInterval);
