c8b20782a1274aef5b2cafb27d24277a82824d28
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-23 00:03:07 +0100

    Comment: use heap? First learn more about it and how to use it in Java

--- java-example/src/learning/mjia/leetcode/grind75/kclosestpointstoorigin/KClosestPointsToOrigin.java
+++ java-example/src/learning/mjia/leetcode/grind75/kclosestpointstoorigin/KClosestPointsToOrigin.java
@@ -8,6 +8,9 @@ public class KClosestPointsToOrigin {
     public int[][] kClosest(int[][] points, int k) {
         // Math.sqrt is unnecessary in our case because we already use integers,
         // so nothing below 0 that might mess up lt or gt signs
+        // Sorting solution
+        // VS
+        // Heap solution?
         Arrays.sort(points, (a, b) -> {
             return a[0] * a[0] + a[1] * a[1] - b[0] * b[0] - b[1] * b[1];
         });
