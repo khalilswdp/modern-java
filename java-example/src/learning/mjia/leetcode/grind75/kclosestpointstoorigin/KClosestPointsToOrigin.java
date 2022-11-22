1f0440b131c175c2d23e2aaf805fa2b2f01d2d49
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-22 23:30:13 +0100

    Minor improvement

--- java-example/src/learning/mjia/leetcode/grind75/kclosestpointstoorigin/KClosestPointsToOrigin.java
+++ java-example/src/learning/mjia/leetcode/grind75/kclosestpointstoorigin/KClosestPointsToOrigin.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 public class KClosestPointsToOrigin {
     public int[][] kClosest(int[][] points, int k) {
-        // add all points distances to the center in a dictionary/ArrayList:
-        // where key is distance and value is the point
-        Arrays.sort(points, (a, b) -> (int) (Math.sqrt(a[0] * a[0] + a[1] * a[1]) - Math.sqrt(b[0] * b[0] + b[1] *b[1])));
-        // sort dictionary by distance and take first k distances
+        // Math.sqrt is unnecessary in our case because we already use integers,
+        // so nothing below 0 that might mess up lt or gt signs
+        Arrays.sort(points, (a, b) -> {
+            return a[0] * a[0] + a[1] * a[1] - b[0] * b[0] - b[1] * b[1];
+        });
+
         List<int[]> result = new ArrayList<>();
         for (int i = 0; i < k; i++) {
             result.add(points[i]);
