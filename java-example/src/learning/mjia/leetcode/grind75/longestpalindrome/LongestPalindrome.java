05639705ce4ea535e821ed196060ad1424e3adfb
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-23 17:42:43 +0100

    Amend fix

--- java-example/src/learning/mjia/leetcode/grind75/longestpalindrome/LongestPalindrome.java
+++ java-example/src/learning/mjia/leetcode/grind75/longestpalindrome/LongestPalindrome.java
@@ -1,17 +1,14 @@
 package learning.mjia.leetcode.grind75.longestpalindrome;
 
-import java.util.Arrays;
-
 public class LongestPalindrome {
     public static void main(String[] args) {
         longestPalindrome("abccccdd");
     }
     public static int longestPalindrome(String s) {
         char[] sChar = s.toCharArray();
-        Arrays.sort(sChar);
         int[] alphabet = new int[128];
-        for (int i = 0; i < sChar.length; i++) {
-            alphabet[sChar[i]]++;
+        for (char currentChar: sChar) {
+            alphabet[currentChar]++;
         }
 
         boolean isThereAnOdd = false;
