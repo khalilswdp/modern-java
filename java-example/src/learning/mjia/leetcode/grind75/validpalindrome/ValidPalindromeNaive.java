013528a2cfbecb9c52e20059b958980d57a33b87
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-25 18:10:38 +0100

    There exists a method for reversing Strings! (through the use of String Builder)

--- java-example/src/learning/mjia/leetcode/grind75/validpalindrome/ValidPalindromeNaive.java
+++ java-example/src/learning/mjia/leetcode/grind75/validpalindrome/ValidPalindromeNaive.java
@@ -2,33 +2,24 @@ package learning.mjia.leetcode.grind75.validpalindrome;
 
 public class ValidPalindromeNaive {
     public boolean isPalindrome(String s) {
-        String stripped = clean(s);
-        String strippedReversed = getReverse(stripped);
-        return stripped.equals(strippedReversed);
-    }
+        String normal = clean(s);
+        String reverse = new StringBuilder(normal).reverse().toString();
 
-    private static String getReverse(String stripped) {
-        byte[] strAsByteArray = stripped.getBytes();
-
-        byte[] result = new byte[strAsByteArray.length];
-
-        for (int i = 0; i < strAsByteArray.length; i++) {
-            result[i] = strAsByteArray[strAsByteArray.length - i - 1];
-        }
-
-        String strippedReversed = new String(result);
-        return strippedReversed;
+        return normal.equals(reverse);
     }
 
     public String clean(String s) {
-        StringBuilder result = new StringBuilder();
-        for (Character c: s.toLowerCase().toCharArray()) {
-            if ((c >= 'a' && c <= 'z' )||(c >= '0' && c <='9')) {
-                result.append(c);
-            } else if (c >= 'A' && c <= 'Z' ) {
-                result.append(Character.toLowerCase(c));
+        StringBuilder sb = new StringBuilder();
+        char[] sChar = s.toCharArray();
+
+        for (char c: sChar) {
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+                sb.append(c);
+            } else if (c >= 'A' && c <= 'Z') {
+                sb.append(Character.toLowerCase(c));
             }
         }
-        return result.toString();
+
+        return sb.toString();
     }
 }
