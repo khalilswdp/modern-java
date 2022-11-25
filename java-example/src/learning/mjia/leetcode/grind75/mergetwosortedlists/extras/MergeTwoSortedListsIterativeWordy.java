c8c64563cfd53d3408317d03069edb6a7adc5831
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-25 16:54:32 +0100

    Improve wordy solution

--- java-example/src/learning/mjia/leetcode/grind75/mergetwosortedlists/extras/MergeTwoSortedListsIterativeWordy.java
+++ java-example/src/learning/mjia/leetcode/grind75/mergetwosortedlists/extras/MergeTwoSortedListsIterativeWordy.java
@@ -4,44 +4,27 @@ import learning.mjia.leetcode.grind75.mergetwosortedlists.ListNode;
 
 public class MergeTwoSortedListsIterativeWordy {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) {
-            return list2;
-        }
-        if (list2 == null) {
-            return list1;
-        }
-        ListNode result, currentIteration;
-        if (list1.val >= list2.val) {
-            currentIteration = list2;
-            list2 = list2.next;
-        } else {
-            currentIteration = list1;
-            list1 = list1.next;
-        }
-        result = currentIteration;
-
-        while (bothNotNull(list1, list2)) {
-            if (list1.val >= list2.val) {
-                currentIteration.next = list2;
-                list2 = list2.next;
-            } else {
-                currentIteration.next = list1;
+        ListNode result = new ListNode(), current = result;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                current = current.next;
                 list1 = list1.next;
+            } else {
+                current.next = list2;
+                current = current.next;
+                list2 = list2.next;
             }
-            currentIteration = currentIteration.next;
         }
 
         if (list1 != null) {
-            currentIteration.next = list1;
-        } else {
-            currentIteration.next = list2;
+            current.next = list1;
+        }
+        if (list2 != null) {
+            current.next = list2;
         }
 
-        return result;
-    }
-
-    private static boolean bothNotNull(ListNode list1, ListNode list2) {
-        return list1 != null && list2 != null;
+        return result.next;
     }
 }
 
