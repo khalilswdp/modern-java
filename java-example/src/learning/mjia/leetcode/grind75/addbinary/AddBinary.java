cd24ec2f433b5943c0c747a5b7ad3ab31d7b3c8b
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-25 15:04:08 +0100

    Second solution

--- java-example/src/learning/mjia/leetcode/grind75/addbinary/AddBinary.java
+++ java-example/src/learning/mjia/leetcode/grind75/addbinary/AddBinary.java
@@ -5,33 +5,18 @@ public class AddBinary {
         char[] aChars = a.toCharArray(), bChars = b.toCharArray();
         StringBuilder result = new StringBuilder();
         int aLength = a.length(), bLength = b.length();
-        boolean carry = false;
+        int carry = 0;
 
         int i = aLength - 1, j = bLength - 1;
 
-        for (; carry || i >= 0 || j >= 0; i--, j--) {
-            char aChar = i >= 0 ? aChars[i] : '0';
-            char bChar = j >= 0 ? bChars[j] : '0';
-            result.insert(0, addBinary(aChar, bChar, carry));
-            carry = getCarry(aChar, bChar, carry);
+        for (; carry != 0 || i >= 0 || j >= 0; i--, j--) {
+            int aChar = i >= 0 ? aChars[i] - '0' : 0;
+            int bChar = j >= 0 ? bChars[j] - '0' : 0;
+            int sum = aChar + bChar + carry;
+            result.insert(0, sum % 2);
+            carry = sum > 1 ? 1 : 0;
         }
 
         return result.toString();
-    }
-
-    public char addBinary(char a, char b, boolean carry) {
-        if (carry) {
-            return a == b ? '1' : '0';
-        } else {
-            return a == b ? '0' : '1';
-        }
-    }
-
-    public boolean getCarry(char a, char b, boolean carry) {
-        if (carry) {
-            return a == '1' || b == '1';
-        } else {
-            return a == '1' && b == '1';
-        }
     }
 }
