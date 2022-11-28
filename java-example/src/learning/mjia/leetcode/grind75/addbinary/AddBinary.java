53b633f8e8b2f96c6732a9cee29e6ed45860d2c7
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-28 17:24:49 +0100

    Add binary minor change

--- java-example/src/learning/mjia/leetcode/grind75/addbinary/AddBinary.java
+++ java-example/src/learning/mjia/leetcode/grind75/addbinary/AddBinary.java
@@ -2,28 +2,24 @@ package learning.mjia.leetcode.grind75.addbinary;
 
 public class AddBinary {
     public String addBinary(String a, String b) {
-        char[] aChars = a.toCharArray(), bChars = b.toCharArray();
-        StringBuilder result = new StringBuilder();
-        int aLength = a.length(), bLength = b.length();
+        StringBuilder res = new StringBuilder();
+
+        int aLen = a.length(), bLen = b.length();
+        char[] aChar = a.toCharArray(), bChar = b.toCharArray();
+
+        int i = aLen - 1, j = bLen - 1;
+
         int carry = 0;
 
-        int i = aLength - 1, j = bLength - 1;
+        for (; i >= 0 || j >= 0 || carry != 0; i--, j--) {
+            int aInt = i >= 0 ? aChar[i] - '0' : 0;
+            int bInt = j >= 0 ? bChar[j] - '0' : 0;
+            carry = aInt + bInt + carry;
+            res.insert(0, carry % 2);
 
-        for (; carry != 0 || i >= 0 || j >= 0; i--, j--) {
-            int aChar = i >= 0 ? aChars[i] - '0' : 0;
-            int bChar = j >= 0 ? bChars[j] - '0' : 0;
-            int sum = aChar + bChar + carry;
-            result.insert(0, sum % 2);
-            carry = sum > 1 ? 1 : 0;
+            carry = carry / 2;
         }
 
-        //              carry = sum / 2;
-        //        }
-        //
-        //        if (carry != 0) {
-        //              result.insert(0, carry);
-        //        }
-
-        return result.toString();
+        return res.toString();
     }
 }
