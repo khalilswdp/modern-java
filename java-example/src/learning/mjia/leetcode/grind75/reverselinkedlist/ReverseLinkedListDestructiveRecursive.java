b9f710982de8b64170bd5310a6f47b24230a8762
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-28 14:47:14 +0100

    Destructive recursive approach

--- java-example/src/learning/mjia/leetcode/grind75/reverselinkedlist/ReverseLinkedListDestructiveRecursive.java
+++ java-example/src/learning/mjia/leetcode/grind75/reverselinkedlist/ReverseLinkedListDestructiveRecursive.java
@@ -1,20 +1,19 @@
 package learning.mjia.leetcode.grind75.reverselinkedlist;
 
 public class ReverseLinkedListDestructiveRecursive {
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList(ListNode first) {
         // This doesn't work, just a place holder
-        if (head == null) {
+        if (first == null) {
             return null;
+        } else if (first.next == null) {
+            return first;
+        } else {
+            ListNode secondToFirst = first.next;
+            first.next = null;
+            ListNode theVeryLast = reverseList(secondToFirst);
+            secondToFirst.next = first;
+
+            return theVeryLast;
         }
-
-        if (head.next == null) {
-            return head;
-        }
-
-        ListNode newHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-
-        return newHead;
     }
 }
