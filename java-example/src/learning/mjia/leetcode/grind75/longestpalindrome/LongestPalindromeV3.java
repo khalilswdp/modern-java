5f4bf3ad831e56f862db8debb31bafb058f1bdfa
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-28 12:26:07 +0100

    Improved solution using bitwise operations, less multiplications conditions and all

---
+++ java-example/src/learning/mjia/leetcode/grind75/longestpalindrome/LongestPalindromeV3.java
@@ -0,0 +1,23 @@
+package learning.mjia.leetcode.grind75.longestpalindrome;
+
+public class LongestPalindromeV3 {
+    public static void main(String[] args) {
+        longestPalindrome("abccccdd");
+    }
+    public static int longestPalindrome(String s) {
+
+        int[] frequency = new int[256];
+        char[] sChar = s.toCharArray();
+        for (char c: sChar) {
+            ++frequency[c];
+        }
+
+        int result = 0, rem = 0;
+        for (int freq: frequency) {
+            result += freq / 2;
+            rem = rem | freq % 2;
+        }
+
+        return 2 * result + rem;
+    }
+}
