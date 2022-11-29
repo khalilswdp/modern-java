9c2fe86d8e59f9fcb4c4d9ae6e5111dce5c0efdb
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-11-29 20:58:50 +0100

    Maximum Depth Of A Binary Tree Iterative

---
+++ java-example/src/learning/mjia/leetcode/grind75/maximumdepthofabinarytree/MaximumDepthOfABinaryTreeIterative.java
@@ -0,0 +1,34 @@
+package learning.mjia.leetcode.grind75.maximumdepthofabinarytree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MaximumDepthOfABinaryTreeIterative {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Deque<TreeNode> toCheck = new ArrayDeque<>();
+        int depth = 0;
+        toCheck.add(root);
+
+        while (!toCheck.isEmpty()) {
+            int numberInLevel = toCheck.size();
+            while (numberInLevel-- > 0) {
+                TreeNode current = toCheck.pop();
+                if (current.right != null) {
+                    toCheck.add(current.right);
+                }
+                if (current.left != null) {
+                    toCheck.add(current.left);
+                }
+            }
+            depth++;
+        }
+
+        return depth;
+    }
+}
+
+
