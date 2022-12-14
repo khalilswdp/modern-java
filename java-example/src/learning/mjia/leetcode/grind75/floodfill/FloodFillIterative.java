29c03421ed087655d981364d54db0843ced5ea9c
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-12-14 16:29:56 +0100

    Remove unnecessarily filling all same distance neighbours at once

--- java-example/src/learning/mjia/leetcode/grind75/floodfill/FloodFillIterative.java
+++ java-example/src/learning/mjia/leetcode/grind75/floodfill/FloodFillIterative.java
@@ -1,36 +1,33 @@
 package learning.mjia.leetcode.grind75.floodfill;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 public class FloodFillIterative {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // Can it be done with BFS?
-        // iterative ?
+        // Iteratively? In the four directions? Use a stack of points visited starting with the center?
         int originalColor = image[sr][sc];
+
         if (originalColor == color) {
             return image;
         }
-        Deque<int[]> pointsToChange = new ArrayDeque<>();
-        pointsToChange.add(new int[] {sr, sc});
 
-        while (!pointsToChange.isEmpty()) {
-            int numberOfPoints = pointsToChange.size();
-            for (int i = 0; i < numberOfPoints; i++) {
-                int[] currentCoordinates = pointsToChange.pop();
-                int srowCoordinate = currentCoordinates[0];
-                int scolumnCoordinate = currentCoordinates[1];
-                if (srowCoordinate >= 0 && srowCoordinate < image.length && scolumnCoordinate >= 0 && scolumnCoordinate < image[0].length && image[srowCoordinate][scolumnCoordinate] == originalColor) {
-                    image[srowCoordinate][scolumnCoordinate] = color;
-                    pointsToChange.add(new int[] {srowCoordinate + 1, scolumnCoordinate});
-                    pointsToChange.add(new int[] {srowCoordinate, scolumnCoordinate + 1});
-                    pointsToChange.add(new int[] {srowCoordinate - 1, scolumnCoordinate});
-                    pointsToChange.add(new int[] {srowCoordinate, scolumnCoordinate - 1});
+        Stack<int[]> toFill = new Stack<>();
+        toFill.push(new int[] { sr, sc });
 
-                }
+        int length = image.length, width = image[0].length;
+        while (!toFill.isEmpty()) {
+            int[] current = toFill.pop();
+            int x = current[0], y = current[1];
+            if (x >= 0 && y >= 0 && x < length && y < width && image[x][y] == originalColor) {
+                image[x][y] = color;
+                toFill.push(new int[] { x + 1, y});
+                toFill.push(new int[] { x, y + 1});
+                toFill.push(new int[] { x - 1, y});
+                toFill.push(new int[] { x, y - 1});
             }
         }
 
         return image;
+
     }
 }
