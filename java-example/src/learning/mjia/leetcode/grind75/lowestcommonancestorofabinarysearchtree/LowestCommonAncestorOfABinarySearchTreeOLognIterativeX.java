56df0380547ab436df8c7f7a0077af0786db0a2f
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-12-14 16:55:14 +0100

    Slightly enhanced

--- java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/LowestCommonAncestorOfABinarySearchTreeOLognIterativeX.java
+++ java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/LowestCommonAncestorOfABinarySearchTreeOLognIterativeX.java
@@ -2,17 +2,15 @@ package learning.mjia.leetcode.grind75.lowestcommonancestorofabinarysearchtree;
 
 public class LowestCommonAncestorOfABinarySearchTreeOLognIterativeX {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode current = root;
-        while (current != null) {
-            if (current.val > p.val && current.val > q.val) {
-                current = current.left;
-            } else if (current.val < p.val && current.val < q.val) {
-                current = current.right;
+        while (true) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
             } else {
-                return current;
+                return root;
             }
         }
-        return null;
     }
 }
 
