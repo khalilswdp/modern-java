3bb930e61b0038f7fd8dda20f755113972eae5be
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-12-14 15:36:26 +0100

    Don't ask me why I wrote this. Just testing things out

---
+++ java-example/src/learning/mjia/leetcode/grind75/binarysearch/BinarySearchRecursiveV2PythonLike.java
@@ -0,0 +1,37 @@
+package learning.mjia.leetcode.grind75.binarysearch;
+
+public class BinarySearchRecursiveV2PythonLike {
+    public static int search(int[] nums, int target) {
+        int numsLength = nums.length;
+        if (numsLength == 0) {
+            return -1;
+        } else if (numsLength == 1 && nums[0] != target) {
+            return -1;
+        }
+
+        int mid = numsLength / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            int[] rightNums = arraySlice(nums, mid + 1, numsLength - 1);
+            int indexAtRight = search(rightNums, target);
+            return indexAtRight == -1 ? -1 : mid + 1 + indexAtRight;
+        } else {
+            int[] leftNums = arraySlice(nums, 0, mid - 1);
+            int indexAtLeft = search(leftNums, target);
+            return indexAtLeft;
+        }
+    }
+
+    public static int[] arraySlice(int[] nums, int start, int end) {
+        int length = end - start + 1;
+        int[] result = new int[length];
+
+        int k = 0;
+        for (int i = start; i <= end && k < length; i++, k++) {
+            result[k] = nums[i];
+        }
+
+        return result;
+    }
+}
