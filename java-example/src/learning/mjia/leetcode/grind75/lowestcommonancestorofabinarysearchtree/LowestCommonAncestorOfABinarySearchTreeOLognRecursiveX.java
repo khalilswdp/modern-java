14081af17edb9a52d8b0131bef4479f0784e91e8
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-12-14 16:51:36 +0100

    Remove unnecessary if null

--- java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/LowestCommonAncestorOfABinarySearchTreeOLognRecursiveX.java
+++ java-example/src/learning/mjia/leetcode/grind75/lowestcommonancestorofabinarysearchtree/LowestCommonAncestorOfABinarySearchTreeOLognRecursiveX.java
@@ -2,9 +2,7 @@ package learning.mjia.leetcode.grind75.lowestcommonancestorofabinarysearchtree;
 
 public class LowestCommonAncestorOfABinarySearchTreeOLognRecursiveX {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
-            return null;
-        } else if (p.val < root.val && q.val < root.val) {
+        if (p.val < root.val && q.val < root.val) {
             return lowestCommonAncestor(root.left, p, q);
         } else if (p.val > root.val && q.val > root.val) {
             return lowestCommonAncestor(root.right, p, q);
