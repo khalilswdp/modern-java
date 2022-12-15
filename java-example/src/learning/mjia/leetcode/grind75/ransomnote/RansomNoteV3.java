b8f8f813527d84a2a6bb738650bb23b8e3ef565f
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-12-15 23:33:11 +0100

    Improve solution that used to not work

--- java-example/src/learning/mjia/leetcode/grind75/ransomnote/RansomNoteV3.java
+++ java-example/src/learning/mjia/leetcode/grind75/ransomnote/RansomNoteV3.java
@@ -2,6 +2,7 @@ package learning.mjia.leetcode.grind75.ransomnote;
 
 public class RansomNoteV3 {
     public boolean canConstruct(String ransomNote, String magazine) {
+        // This is more of specific java knowledge
         int[] lastOccurrence = new int[26];
 
         int rNLength = ransomNote.length(), mLength = magazine.length();
@@ -13,11 +14,11 @@ public class RansomNoteV3 {
         char[] rNChars = ransomNote.toCharArray();
 
         for (char rN: rNChars) {
-            int index = magazine.indexOf(rN, lastOccurrence[rN - 'a'] + 1);
+            int index = magazine.indexOf(rN, lastOccurrence[rN - 'a']);
             if (index == -1) {
                 return false;
             }
-            lastOccurrence[rN] = index;
+            lastOccurrence[rN - 'a'] = index + 1;
         }
         return true;
     }
