8642cdfb43454319fc160bae6462b782f9d305d9
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-12-15 23:53:30 +0100

    Change Climbing Stairs to become more readable

--- java-example/src/learning/mjia/leetcode/grind75/climbingstairs/ClimbingStairs.java
+++ java-example/src/learning/mjia/leetcode/grind75/climbingstairs/ClimbingStairs.java
@@ -2,19 +2,16 @@ package learning.mjia.leetcode.grind75.climbingstairs;
 
 public class ClimbingStairs {
     public int climbStairs(int n) {
-        int[] results = new int[n + 1];
-        int i = 0;
-        results[n - i] = 1;
-        i++;
-        results[n - i] = 1;
-        i++;
+        int[] dictionary = new int[n + 2];
+        dictionary[1] = 1;
+        dictionary[2] = 2;
+
+        int i = 3;
         while (i <= n) {
-            int left = results[n - i + 1];
-            int right = results[n - i + 2];
-            int result = left + right;
-            results[n - i] = result;
+            dictionary[i] = dictionary[i - 1] + dictionary[i - 2];
             i++;
         }
-        return results[0];
+
+        return dictionary[n];
     }
 }
